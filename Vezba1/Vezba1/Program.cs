f11debbc498bbac543ba5f25d2641748009ab033
Aleksandar Ilijevski <privatesteam@hotmail.com>
2019-12-14 20:55:30 +0100

    Changes to code

---
+++ Vezba1/Vezba1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vezba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
